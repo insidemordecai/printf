367af5c575c24fe74b73d310c7730659b7da82e6
Mordecai K <mordecai.kipngetich@gmail.com>
2022-10-17 15:35:28 +0300

    update: move toBin to functions.c from convert.c

--- functions.c
+++ functions.c
@@ -1,3 +1,5 @@
+/* FILE CONTAINS: printString, printInt, toBin */
+
 #include "main.h"
 
 /**
@@ -46,6 +48,42 @@ int printInt(long int integer)
 
 	_putchar((integer % 10) + '0');
 	length++;
+
+	return (length);
+}
+
+/**
+ * toBin - convert unsigned int to binary (base 2)
+ * @value: number to convert
+ * Return: number of binary characters
+ */
+int toBin(unsigned int value)
+{
+	int binary[sizeof(unsigned int) * 8], mod, i, length = 0;
+
+	if (value == 0)
+	{
+		length += _putchar('0');
+		return (length);
+	}
+
+	if (value == 1)
+	{
+		length += _putchar('1');
+		return(length);
+	}
+
+	mod = 0;
+
+	while (value > 0)
+	{
+		binary[mod] = '0' + (value % 2);
+		value /= 2;
+		mod++;
+	}
+
+	for (i = mod - 1; i >= 0; i--)
+		length += _putchar(binary[i]);
 
 	return (length);
 }
